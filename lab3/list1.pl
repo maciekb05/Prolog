872ece62fdf95a120b8bfabb14440a2226c05615
maciekb05 <maciej.bielech@gmail.com>
2018-03-18 12:40:35 +0100

    Lab3

---
+++ lab3/list1.pl
@@ -0,0 +1,78 @@
+porownaj([_,_,C,D|_]):-
+	C = D.
+	
+zamien([A,B,C,D|Rest],X):-
+	X = [A,B,D,C|Rest].
+	
+nalezy(X,[X|_]).
+nalezy(X,[_|Tail]) :-
+	nalezy(X,Tail).
+	
+dlugosc([],0).
+dlugosc([_|Tail],Length):-
+	dlugosc(Tail,X),
+	Length is X + 1.
+	
+a2b([],[]).
+a2b([a|Ta],[b|Tb]):-
+	a2b(Ta,Tb).
+	
+sklej([],X,X).
+sklej([X|L1],L2,[X|L3]):-
+	sklej(L1,L2,L3).
+	
+nalezy1(X,L) :-
+	sklej(_,[X|_],L).
+	
+ostatni(X,[X]).
+ostatni(X,[_|T]):-
+	ostatni(X,T).
+	
+ostatni1(X,L):-
+	sklej(_,[X],L).
+	
+dodaj(X,L,[X|L]).
+
+usun(X,[X|Tail],Tail).
+usun(X,[Y|Tail],[Y|List]):-
+	usun(X,Tail,List).
+	
+wstaw(X,L,Duza) :-
+	usun(X,Duza,L).
+	
+nalezy2(X,L) :-
+	usun(X,L,_).
+
+zawiera(S,L) :-
+	sklej(_,L2,L),
+	sklej(S,_,L2).
+
+permutacja([],[]).
+permutacja([X|L],P) :-
+	permutacja(L,L1),
+	wstaw(X,L1,P).
+ 
+permutacja2([],[]).
+permutacja2(L,[X|P]) :-
+	usun(X,L,L1),
+	permutacja2(L1,P).
+	
+odwroc([],[]).
+odwroc([H|T],L) :-
+	odwroc(T,R),
+	sklej(R,[H],L).
+
+wypisz([H|T]) :-
+	put(H), wypisz(T).
+wypisz([]).
+
+plural(Noun, PL) :- 
+	name(Noun, L), 
+	name(s,T), 
+	append(L,T, NL), 
+	name(PL, NL).
+
+
+
+	
+
